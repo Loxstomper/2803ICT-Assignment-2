4627282069dacfd8e04fd783f542f711a3e086cb
loxstomper <lochieashcroft@gmail.com>
2018-09-10 19:23:13 +1000

    pushing to work on desktop

--- client.c
+++ client.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv)
 {
+    char user_input[1000];
+    unsigned long int number;
+    int n_requests = 0;
+
+    while (1)
+    {
+        printf("> ");
+        fgets(user_input, 1000, stdin);
+
+        if (user_input[0] == 'q')
+        {
+            printf("Goodbye ( ﾟ▽ﾟ)/ \n");
+            break;
+        }
+
+        if (n_requests == 10)
+        {
+            printf("System is busy! \n");
+            continue;
+        }
+
+        n_requests ++;
+
+        /* convert to number */
+        number = strtoul(user_input, NULL, 0);
+
+        printf("NUMBER: %ul \n", number);
+        /* store in shared memory */
+
+
+    }
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 
+void rotate(unsigned long int number)
+{
+    printf("ORIGINAL: %lu \n", number);
+
+    unsigned long int rotated;
+
+    for (int i = 0; i < 32; i ++)
+    {
+        rotated = (number >> i) | (number << 32 - i);
+        printf("ROTATE: %d : %d \n", i, rotated);
+    }
+}
+
+void build_rotations(unsigned long int* rotations)
+{
+    rotations = malloc(sizeof(unsigned long int*) * 10);
+
+    for (int i = 0; i < 10; i ++)
+    {
+        rotations[i] = malloc(sizeof(unsigned long int) * 32);
+    }
+}
+
+void print_
+
 int main(int argc, char** argv)
 {
+    unsigned long int number;
+
+    unsigned long int* rotations;
+    build_rotations(&rotations);
+
+
+
+    number = 4;
+
+    rotate(number);
+
     return 0;
 }
