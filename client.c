f83ad0cfc80225cf42f210916b5a3dcdc7c7a61e
lox <lox.ash@hotmail.com>
2018-09-16 21:53:38 +1000

    Workign on the shared memory for the client

--- client.c
+++ client.c
@@ -4,15 +4,78 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+#define NUMBER_KEY 10000
+#define CLIENT_FLAG_KEY 10001   
+#define SERVER_FLAG_KEY 10002   
+
+typedef unsigned long int uli;
+
+
+// check the query status and display any new info
+void get_queries()
+{
+
+}
+
+void get_shared_memory(uli** number, int** client_flag, int** server_flag)
+{
+    key_t key = ftok("shmfile", NUMBER_KEY);
+    int shmid;
+    
+    if ((shmid = shmget(key, sizeof(unsigned long int), 0666 | IPC_CREAT)) == -1)
+    {
+        perror("SHMGET, number");
+        exit(1);
+
+    }
+
+    *number = (unsigned long int*) shmat(shmid, (void*)0, 0);
+
+    key = ftok("shmfile", CLIENT_FLAG_KEY);
+
+    if ((shmid = shmget(key, sizeof(int), 0666 | IPC_CREAT)) == -1)
+    {
+        perror("SHMGET, client flag");
+        exit(1);
+
+    }
+
+    *client_flag = (int* ) shmat(shmid, (void*)0, 0);
+
+    key = ftok("shmfile", SERVER_FLAG_KEY);
+
+    // if ((shmid = shmget(key, sizeof(int) * 10, 0666 | IPC_CREAT)) == -1)
+    // {
+    //     perror("SHMGET, server flag");
+    //     exit(1);
+
+    // }
+
+    // *server_flag = (int* ) shmat(shmid, (void*)0, 0);
+}
+
+void detach_shared_memory(uli** number)
+{
+    shmdt(&number);
+}
+
+
 int main(int argc, char** argv)
 {
     char user_input[1000];
-    unsigned long int number;
     int n_requests = 0;
 
-    key_t key = ftok("shmfile", 1337);
-    int shmid = shmget(key, sizeof(unsigned long int), 0666 | IPC_CREAT);
-    unsigned long int* shared_number;
+    uli* number;
+    int* client_flag;
+    int* server_flag;
+
+    get_shared_memory(&number, &client_flag, &server_flag);
+
+    *client_flag = 0;
+
+
+    int used_slot;
+
 
     while (1)
     {
@@ -32,20 +95,29 @@ int main(int argc, char** argv)
         }
 
 
-        n_requests ++;
+        // blocking, will wait for server to read
+        while (*client_flag == 1)
+        {
 
-        /* convert to number */
-        number = strtoul(user_input, NULL, 0);
+        }
 
-        printf("NUMBER: %lu \n", number);
-        /* store in shared memory */
-        shared_number = (unsigned long int*) shmat(shmid, (void*)0, 0);
-        *shared_number = number;
+        *number =  strtoul(user_input, NULL, 0);
+        *client_flag = 1;
+        printf("NUMBER: %lu \n", *number);
 
-        // detach boiii
-        shmdt(shared_number);
+        // wait for the server to read number
+        while (*client_flag == 1)
+        {
+
+        }
+
+        // server writes the slot the number is in into the number variable
+        used_slot = *number;
 
     }
+
+    // detach boiii
+    detach_shared_memory(&number);
 
     return 0;
 }
